a88c6e3ac66e9c2d7b9be170e50f9cac93906916
Tu Nguyen <nvminhtu@gmail.com>
2015-10-15 17:02:22 +0700

    Add needed plugins

---
+++ wp-loadmore/js/load-posts.js
@@ -0,0 +1,85 @@
+(function($){
+  var Index = $.index = (function(){
+    /* field
+    ----------------------------------------*/
+	
+	var 
+    // The number of the next page to load (/page/x/).
+    pageNum = parseInt(pcode.startPage) + 1,
+	  // The maximum number of pages the current query can return.
+	  max = parseInt(pcode.maxPages),
+	  // The link of the next page of posts.
+	  nextLink = pcode.nextLink;
+  
+    /* constructor
+    ----------------------------------------*/
+    function init(){
+      setUp();
+      loadMore();
+
+      // $(window).scroll(function(){
+
+      // });
+    }
+    /**
+     ** Load Setup
+    **/
+    function setUp(){
+      if(pageNum <= max) {
+        // Insert the "More Posts" link.
+        $('#content')
+          .append('<div class="pbd-alp-placeholder-'+ pageNum +'"></div>')
+          .append('<p id="pbd-alp-load-posts"><a href="#">Load More Posts</a></p>');
+          
+        // Remove the traditional navigation.
+        $('.navigation').remove();
+      }
+    }
+    /**
+     ** Load more Posts
+    **/
+    function loadMore(){
+      $('#pbd-alp-load-posts a').click(function() {
+        // Are there more posts to load?
+        if(pageNum <= max) {
+        
+          // Show that we're working.
+          $(this).text('Loading posts...');
+          
+          $('.pbd-alp-placeholder-'+ pageNum).load(nextLink + ' .post',
+            function() {
+              
+              // Update page number and nextLink.
+              pageNum++;
+              nextLink = nextLink.replace(/\/page\/[0-9]?/, '/page/'+ pageNum);
+              
+              // Add a new placeholder, for when user clicks again.
+              $('#pbd-alp-load-posts')
+                .before('<div class="pbd-alp-placeholder-'+ pageNum +'"></div>')
+              
+              // Update the button message.
+              if(pageNum <= max) {
+                $('#pbd-alp-load-posts a').text('Load More Posts');
+              } else {
+                $('#pbd-alp-load-posts a').text('No more posts to load.');
+              }
+            }
+          );
+        } else {
+          $('#pbd-alp-load-posts a').append('.');
+        } 
+        
+        return false;
+      });
+    }
+    
+    /* access control
+    ----------------------------------------*/
+    return {
+      init : init
+    }
+  })();
+  // document.ready
+  $(Index.init);
+
+})(jQuery);
---
+++ wp-loadmore/pbd-ajax-load-posts.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Plugin Name: AJAX Load Posts
+ * Plugin URI: 
+ * Description: Load more post with Ajax
+ * Version: 0.1
+ * Author: Tu Nguyen
+ * Author URI: 
+ */
+ 
+ /**
+  * Initialization. Add our script if needed on this page.
+  */
+ function pcode_init() {
+ 	global $wp_query;
+ 
+ 	// Add code to index pages.
+ 	if( !is_singular() ) {	
+ 		// Queue JS and CSS
+ 		wp_enqueue_script(
+ 			'pcode-load-posts',
+ 			plugin_dir_url( __FILE__ ) . 'js/load-posts.js',
+ 			array('jquery'),
+ 			'1.0',
+ 			true
+ 		);
+ 		
+ 		wp_enqueue_style(
+ 			'pcode-style',
+ 			plugin_dir_url( __FILE__ ) . 'css/style.css',
+ 			false,
+ 			'1.0',
+ 			'all'
+ 		);
+ 		
+ 		// What page are we on? And what is the pages limit?
+ 		$max = $wp_query->max_num_pages;
+ 		$paged = ( get_query_var('paged') > 1 ) ? get_query_var('paged') : 1;
+ 		
+ 		// Add some parameters for the JS.
+ 		wp_localize_script(
+ 			'pcode-load-posts',
+ 			'pcode',
+ 			array(
+ 				'startPage' => $paged,
+ 				'maxPages' => $max,
+ 				'nextLink' => next_posts($max, false)
+ 			)
+ 		);
+ 	}
+ }
+ add_action('template_redirect', 'pcode_init');
+ 
+ ?>
